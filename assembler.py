677c55e599d2069c82af6579add789cfbfd53881
pedroernesto00 <39915443+pedroernesto00@users.noreply.github.com>
2019-05-03 20:56:49 -0300

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -17,23 +17,25 @@ lista_bytes = []   # lista que armazena os endereços das labels, variaveis, ins
 tela_erro = ""     
 
 
-contador_linha = 0
-contador_byte = 0
+contador_linha = 0 
+contador_byte = 0  
 
-proxima_variavel = 0
+proxima_variavel = 0 
 
-def add_erro(tipo_erro, linha_erro):
-    global contador_linha
+# funções
+def add_erro(tipo_erro, linha_erro):        # adiciona mensagem de erro e mostra a linha que contem o erro  
+    global contador_linha, tela_erro
 
-    return ("Há um erro na linha " + str(linha_erro) + ":" + tipo_erro)
+    tela_erro += ("Há um erro na linha " + str(linha_erro) + ": " + tipo_erro + "\n")
+    return tela_erro
 
-def checar_comentario(string):
+def checar_comentario(string):              # checa se a linha é um comentário
     try:
         return string.startswith("//")
     except TypeError:
         raise TypeError
 
-def checar_label(string):
+def checar_label(string):                   # checa se um elemento é uma label
     global contador_linha, dic_variaveis, dic_instrucoes
 
     if string in dic_variaveis.keys():
@@ -42,12 +44,12 @@ def checar_label(string):
     else:
         return string not in dic_instrucoes.keys() and string.replace("_", "").isalnum()
 
-def add_label(label):
+def add_label(label):                        # sendo uma label, é adicionada ao dicionário de labels
     global contador_byte, dic_labels
 
     dic_labels[label] = contador_byte + 1
 
-def checar_variavel_valida(string):
+def checar_variavel_valida(string):          # checa se um elemento é uma variavel válida
     global dic_labels, contador_linha
 
     if string in dic_labels.keys():
@@ -56,18 +58,18 @@ def checar_variavel_valida(string):
     else:
         return string[0].isalpha() and string.replace("_","").isalnum()
 
-def checar_instrucao_valida(instrucao):
+def checar_instrucao_valida(instrucao):       # checa se a instrução está no dicionario de instruções, definido no inicio
     global dic_instrucoes
 
     return instrucao in dic_instrucoes.keys()
 
-def add_instrucao(instrucao):
+def add_instrucao(instrucao):                  # endereço de memoria da instrução é adicionada à lista de bytes
     global contador_byte,  dic_instrucoes
 
     lista_bytes.append(dic_instrucoes[instrucao][0])
     contador_byte += 1
 
-def checar_operando_valido(instrucao, operando, tipo_operando, numero_operandos):
+def checar_operando_valido(instrucao, operando, tipo_operando, numero_operandos):             # checa se o operando da instrução é válido
     global dic_variaveis
     
     if numero_operandos == len(operando):
@@ -97,8 +99,8 @@ def checar_operando_valido(instrucao, operando, tipo_operando, numero_operandos)
     else: 
         return False
 
-def add_operando(instrucao,operando, tipo_operando, numero_operandos):
-    global dic_variaveis, contador_byte, proxima_variavel
+def add_operando(instrucao,operando, tipo_operando, numero_operandos):              # o endereço do operando é adicionado à lista de bytes e
+    global dic_variaveis, contador_byte, proxima_variavel                           # dependendo do tipo do operando, é incrementado 1 ou 2 bytes ao contador de bytes
 
     for i in range(0, numero_operandos, 1):
         if tipo_operando[i] == "varnum":
@@ -109,9 +111,7 @@ def add_operando(instrucao,operando, tipo_operando, numero_operandos):
             if operando[i] not in dic_variaveis.keys():
                 dic_variaveis[operando[i]] = proxima_variavel
                 proxima_variavel += 1
-            else:
-                add_erro("Variável já existente", contador_linha)
-            
+                        
             lista_bytes.append(dic_variaveis[operando[i]])
             contador_byte += 1
             
@@ -119,7 +119,7 @@ def add_operando(instrucao,operando, tipo_operando, numero_operandos):
             lista_bytes.append(int(operando[i]))
             contador_byte += 1            
 
-        elif tipo_operando[i] == "disp" or tipo_operando[i] == "index": #Nao entendo
+        elif tipo_operando[i] == "disp" or tipo_operando[i] == "index": 
             lista_bytes.append(int(operando[i]) & 0xff)
             lista_bytes.append(int(operando[i]) >> 8)
             contador_byte += 2
@@ -127,30 +127,30 @@ def add_operando(instrucao,operando, tipo_operando, numero_operandos):
             lista_bytes.append([operando[i], contador_byte])
             contador_byte += 2
 
-def gerar_executavel():
+def gerar_executavel():                             # criando o arquivo executável
     global lista_bytes, contador_linha, contador_byte
 
-    bytes_para_gravacao = bytearray()
+    bytes_para_gravacao = bytearray()               # array de bytes que será escrita no arquivo
     
-    tamanho_arquivo = (contador_byte + 20).to_bytes(4, "little", signed = True)
+    tamanho_arquivo = (contador_byte + 20).to_bytes(4, "little", signed = True)     # arquivo deve iniciar com 4 bytes indicando o tamanho do programa
     
     bytes_para_gravacao += tamanho_arquivo 
 
-    bytes_inicializacao = [0x7300, 0x0006, 0x1001, 0x0400, 0x1001 + len(dic_variaveis.keys())]
+    bytes_inicializacao = [0x7300, 0x0006, 0x1001, 0x0400, 0x1001 + len(dic_variaveis.keys())]      # bytes pré-definidos de inicialização 
     
     try:
         for byte in bytes_inicializacao:
-            bytes_para_gravacao += byte.to_bytes(4, "little", signed = True)
+            bytes_para_gravacao += byte.to_bytes(4, "little", signed = True)         # adicionando os bytes de inicialização
 
-        for byte in lista_bytes:
-            if type(byte) == list:
+        for byte in lista_bytes:                          
+            if type(byte) == list:                                                   # checa se o operando é do tipo offset
                 if byte[0] not in dic_labels.keys():
-                    add_erro("Label não encontrada", contador_linha)
+                    add_erro("Label não encontrada", contador_linha)                 # erro se a label não existe  
                 else:
-                    byte_label = dic_labels[byte[0]] - byte[1]
-                    bytes_para_gravacao += byte_label.to_bytes(2, "big", signed = True)
+                    byte_label = dic_labels[byte[0]] - byte[1]                       # deve-se pegar a distância entre o endereço da label e da instrução em que a label é operando
+                    bytes_para_gravacao += byte_label.to_bytes(2, "big", signed = True)   # a distância é adicionada
             else:
-                bytes_para_gravacao.append(byte)
+                bytes_para_gravacao.append(byte)                                     # se não é label, é adicionado à array 
     except Exception:
         print("Erro ao gerar o arquivo")
         print(tela_erro)
@@ -162,28 +162,31 @@ def gerar_executavel():
 def main():
     global contador_linha
 
-    try:
+    try:                                                         # tenta abrir o arquivo
         arq_asm = open(sys.argv[1], 'r')
-    except IndexError:
+    except IndexError:                                           # não é passado parametro para leitura
         print("Não há arquivo para abrir")
         raise IndexError
-    except IOError:
+    except IOError:                                              # o arquivo passado não existe 
         print("Não é possível abrir o arquivo")
         raise IOError
 
     for linha in arq_asm:
-        linha_lista = linha.lower().split()
+        contador_linha += 1
+
+        linha_lista = linha.lower().split()                       # não é case sensitive
         
-        try:
-            if checar_label(linha_lista[0]):
-                add_label(linha_lista[0])
+        try:                                                       
+            if checar_label(linha_lista[0]):                      # vai checar se a linha possui label
+                add_label(linha_lista[0])                         # se possuir, label é adicionada ao seu dicionario
                 del linha_lista[0]
-        except IndexError:
+        except IndexError:                                        # caso a linha não possua label, passar-se-á para a próxima
             pass
 
         if linha_lista != [] and not checar_comentario(linha_lista[0]):
             instrucao = linha_lista[0]
-
+            
+            # a partir daqui, cada instrução é checada e, sendo válida, faz-se a checagem do(s) operando(s)
             if checar_instrucao_valida(instrucao):
                 tipo_operando = dic_instrucoes[instrucao][1]
                 numero_operandos = len(tipo_operando)
@@ -199,16 +202,16 @@ def main():
             else:
                 add_erro("Instrução inválida", contador_linha)
 
-    arq_asm.close()
-    print(lista_bytes)         
+    arq_asm.close()  
+
     if tela_erro == "":
         gerar_executavel()
     
     else:
         print(tela_erro)
             
-if __name__ == '__main__':
-    main()    
+if __name__ == '__main__':                      # looping
+    main()   
 
 
 
